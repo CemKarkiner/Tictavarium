adfd681452335525f36b7e32209f13eb75ad87b7
Cem Karkıner <138476660+CemKarkiner@users.noreply.github.com>
2025-07-17 10:24:31 +0300

    Delete src directory

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { MainPage } from "./screens/MainPage";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <MainPage />
-  </StrictMode>,
-);
